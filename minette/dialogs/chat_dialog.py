57c8946dbd6325937c01a81d812d905755b7b5b3
uezo <uezo@uezo.net>
2017-07-19 19:47:07 +0900

    #10 ChatDialog response was sometimes garbling

--- minette/dialogs/chat_dialog.py
+++ minette/dialogs/chat_dialog.py
@@ -1,17 +1,22 @@
 """ Japanese chatting """
 import logging
+import traceback
+from time import time
 from configparser import ConfigParser
 import json
 from pytz import timezone
+from datetime import datetime
 import requests
 from minette.session import Session
 from minette.dialog import Message, DialogService
+from minette.util import date_to_str
 
 class ChatDialogService(DialogService):
-    def __init__(self, request:Message, session:Session, logger:logging.Logger=None, config:ConfigParser=None, tzone:timezone=None, api_key="", replace_values:dict=None):
-        super().__init__(request=request, session=session, logger=logger, config=config, tzone=tzone)
+    def __init__(self, request, session, logger=None, config=None, tzone=None, connection=None, api_key="", replace_values=None, debug=False):
+        super().__init__(request=request, session=session, logger=logger, config=config, tzone=tzone, connection=connection)
         self.api_key = api_key
         self.replace_values = replace_values if replace_values else {}
+        self.debug = debug
 
     def process_request(self):
         chat_req = {
@@ -28,7 +33,37 @@ class ChatDialogService(DialogService):
         for k, v in self.replace_values.items():
             chat_str = chat_str.replace(k, v)
         self.session.data = chat_str
+        self.debug_chat_message(chat_res)
 
-    def compose_response(self) -> Message:
+    def compose_response(self):
         self.session.keep_mode = True if self.session.mode == "srtr" else False
         return self.request.get_reply_message(str(self.session.data))
+
+    def debug_chat_message(self, chat_json):
+        """
+        :param chat_json: JSON to log
+        """
+        if self.debug is False:
+            return
+        try:
+            start_time = time()
+            chat_json_str = json.dumps(chat_json)
+            now = datetime.now(self.timezone)
+            cursor = self.connection.cursor()
+            sql = "insert into chatlog(timestamp, overhead, text) values (%s,%s,%s)"
+            overhead = int((time() - start_time) * 1000)
+            cursor.execute(sql, (date_to_str(now, True), overhead, chat_json_str))
+            self.connection.commit()
+        except Exception as ex:
+            self.logger.error("Error occured in logging chat message for debug: " + str(ex) + "\n" + traceback.format_exc())
+
+    @classmethod
+    def prepare_table(cls, connection_provider):
+        """
+        :param connection_provider: MySQLConnectionProvider to create table if not existing
+        :type connection_provider: MySQLConnectionProvider
+        """
+        connection = connection_provider.get_connection()
+        cursor = connection.cursor()
+        cursor.execute("create table chatlog(timestamp DATETIME, overhead INT, text VARCHAR(4000))")
+        connection.commit()
